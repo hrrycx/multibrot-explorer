bd7721570900a69ce71f230015634e4257611ab8
hrrycx <2005hcox@gmail.com>
2023-08-07 15:59:07 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,346 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use colorsys::Hsl;
+use colorsys::Rgb;
+use eframe::egui;
+use eframe::IconData;
+use egui::*;
+use egui_extras::RetainedImage;
+use rayon::iter::IntoParallelIterator;
+use rayon::iter::ParallelIterator;
+use std::time::Instant;
+pub const WIDTH: i32 = 1976 / 2;
+pub const HEIGHT: i32 = 1792 / 2;
+//TODO
+// allow negative powers (look on wikipedia, theres a cool formula thing that you dont understand)
+// better colours
+// add option to show axes
+// styling
+fn main() -> Result<(), eframe::Error> {
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(12)
+        .build_global()
+        .unwrap();
+    let mut native_options = eframe::NativeOptions::default();
+    native_options.icon_data = Some(IconData {
+        rgba: mandelbrot(coord { x: -0.765, y: 0. }, 1., 250., 2, 256, 256),
+        width: 256,
+        height: 256,
+    });
+    native_options.maximized = true;
+
+    eframe::run_native(
+        "Mandelbrot Explorer",
+        native_options,
+        Box::new(|_cc| Box::<Content>::default()),
+    )
+}
+struct Content {
+    center: coord,
+    zoom: f64,
+    image: RetainedImage,
+    time: f64,
+    maxitr: i32,
+    exponent: i32,
+    prev: (coord, f64, i32, i32),
+}
+impl Default for Content {
+    fn default() -> Self {
+        Self {
+            center: coord { x: -0.765, y: 0. },
+            zoom: 1.,
+            image: RetainedImage::from_color_image(
+                "mandel",
+                ColorImage::from_rgba_unmultiplied(
+                    [WIDTH as usize, HEIGHT as usize],
+                    &mandelbrot(coord { x: -0.765, y: 0. }, 1., 300., 2, WIDTH, HEIGHT),
+                ),
+            ),
+            time: 50000000.,
+            maxitr: 300,
+            exponent: 2,
+            prev: (coord { x: -0.765, y: 0. }, 1., 0, 2),
+        }
+    }
+}
+
+impl eframe::App for Content {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let frame = Frame::side_top_panel(&ctx.style()).inner_margin(0.0);
+        egui::SidePanel::left("my_left_panel")
+            .frame(frame)
+            .exact_width(WIDTH as f32 + 1.0)
+            .resizable(false)
+            .show(ctx, |ui| {
+                ui.vertical(|ui| {
+                    self.image.show(ui);
+                    let pos = ctx.pointer_hover_pos();
+                    if pos.is_some() {
+                        let pos = pos.unwrap();
+                        if pos.x < WIDTH as f32 && pos.y < HEIGHT as f32 {
+                            let x = px(pos.x as f64, self.zoom, self.center.x, WIDTH);
+                            let y = py(pos.y as f64, self.zoom, self.center.y, HEIGHT);
+
+                            ui.label(format!("pointer x: {}", x));
+                            ui.label(format!("pointer y: {}", y));
+                            let (iterations, _) =
+                                mandelcomp(x, y, self.maxitr as f64, self.exponent);
+                            if iterations == self.maxitr {
+                                ui.label(format!(
+                                    "this point stays bounded (in {} iterations)",
+                                    self.maxitr
+                                ));
+                            } else {
+                                ui.label(format!(
+                                    "this point escapes in : {} iterations",
+                                    iterations
+                                ));
+                            }
+                        }
+                    }
+                });
+            });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.horizontal_wrapped(|ui| {
+                ui.style_mut().override_text_style = Some(egui::TextStyle::Heading);
+                ui.label(format!(
+                    "frame render time: {:.1}ms",
+                    (self.time / 1000000.)
+                ));
+                let fps = 1000000000. / (self.time);
+                let color = Color32::from_rgb(
+                    (-(fps - 20.) * 255. / 5.) as u8,
+                    ((fps - 10.) * 255. / 5.) as u8,
+                    0,
+                );
+                ui.colored_label(color, format!("({:.1} fps)", fps));
+            });
+            ui.separator();
+            ui.style_mut().spacing.item_spacing = Vec2 { x: 10., y: 15. };
+            ui.add(egui::Slider::new(&mut self.maxitr, 0..=15000).text("max iterations"));
+            ui.add(egui::Slider::new(&mut self.exponent, 1..=100).text("exponent"));
+            ui.add(
+                egui::DragValue::new(&mut self.center.x)
+                    .speed(0.1 * self.zoom)
+                    .prefix("x: "),
+            );
+            ui.add(
+                egui::DragValue::new(&mut self.center.y)
+                    .speed(0.1 * self.zoom)
+                    .prefix("y: "),
+            );
+            ui.add(
+                egui::Slider::new(&mut self.zoom, 1.0..=0.000000000001)
+                    .logarithmic(true)
+                    .text("scale"),
+            );
+
+            let new: (coord, f64, i32, i32) = (self.center, self.zoom, self.maxitr, self.exponent);
+            if new != self.prev {
+                self.render();
+            }
+            if ctx.input(|i| i.pointer.is_decidedly_dragging()) {
+                let origin = ctx.input(|i| i.pointer.press_origin());
+                if origin.is_some() {
+                    let origin = origin.unwrap();
+                    let current = ctx.input(|i| i.pointer.interact_pos()).unwrap();
+                    if origin.x < WIDTH as f32
+                        && origin.y < HEIGHT as f32
+                        && current.x < WIDTH as f32
+                        && current.y < HEIGHT as f32
+                        && origin.y > 0.
+                        && current.y > 0.
+                    {
+                        let dy = ctx.input(|i| i.pointer.delta()).y as f64;
+                        let dx = ctx.input(|i| i.pointer.delta()).x as f64;
+                        self.center.x -= ((2.47) / WIDTH as f64) * self.zoom * dx;
+                        self.center.y -= ((2.24) / HEIGHT as f64) * self.zoom * dy;
+                        self.render()
+                    }
+                }
+            }
+            if ctx.input(|i| i.key_pressed(Key::A)) {
+                self.center.x -= 0.1 * self.zoom;
+                self.render();
+            }
+            if ctx.input(|i| i.key_pressed(Key::W)) {
+                self.center.y -= 0.1 * self.zoom;
+                self.render();
+            }
+            if ctx.input(|i| i.key_pressed(Key::S)) {
+                self.center.y += 0.1 * self.zoom;
+                self.render();
+            }
+            if ctx.input(|i| i.key_pressed(Key::D)) {
+                self.center.x += 0.1 * self.zoom;
+                self.render();
+            }
+            let current = ctx.input(|i| i.pointer.hover_pos());
+            if current.is_some() {
+                let current = current.unwrap();
+                if current.x < WIDTH as f32 && current.y < HEIGHT as f32 && current.y > 0. {
+                    if ctx.input(|i| i.scroll_delta.y > 0.) {
+                        self.zoom *= 0.5;
+                        self.render();
+                    }
+                    if ctx.input(|i| i.scroll_delta.y < 0.) {
+                        self.zoom *= 2.;
+                        self.render();
+                    }
+                }
+            }
+            self.prev = new;
+        });
+    }
+}
+impl Content {
+    fn render(&mut self) {
+        let now = Instant::now();
+        self.image = RetainedImage::from_color_image(
+            "mandel",
+            ColorImage::from_rgba_unmultiplied(
+                [WIDTH as usize, HEIGHT as usize],
+                &mandelbrot(
+                    self.center,
+                    self.zoom,
+                    self.maxitr as f64,
+                    self.exponent,
+                    WIDTH,
+                    HEIGHT,
+                ),
+            ),
+        );
+        self.time = now.elapsed().as_nanos() as f64;
+    }
+}
+#[derive(Copy, Clone, PartialEq)]
+struct coord {
+    pub x: f64,
+    pub y: f64,
+}
+#[derive(Copy, Clone, Debug)]
+struct complex {
+    re: f64,
+    im: f64,
+}
+fn cmul(z1: complex, z2: complex) -> complex {
+    complex {
+        re: z1.re * z2.re - z1.im * z2.im,
+        im: z1.re * z2.im + z1.im * z2.re,
+    }
+}
+fn cadd(z1: complex, z2: complex) -> complex {
+    complex {
+        re: z1.re + z2.re,
+        im: z1.im + z2.im,
+    }
+}
+fn crec(z: complex) -> complex {
+    let den = z.re * z.re + z.im * z.im;
+    return complex {
+        re: z.re / den,
+        im: -z.im / den,
+    };
+}
+fn cpow(z: complex, n: i32) -> complex {
+    if (n < 0) {
+        return cpow(crec(z), -n);
+    }
+    if (n == 0) {
+        return complex { re: 1., im: 0. };
+    }
+    if (n == 1) {
+        return z;
+    }
+    if (n % 2 == 0) {
+        return cpow(cmul(z, z), n / 2);
+    } else {
+        return cmul(z, cpow(cmul(z, z), (n - 1) / 2));
+    }
+}
+fn mandelbrot(
+    center: coord,
+    scale: f64,
+    maxitr: f64,
+    exponent: i32,
+    width: i32,
+    height: i32,
+) -> Vec<u8> {
+    let buf: Vec<u8> = (0..height)
+        .into_par_iter()
+        .map(|y| renderline(y as u32, scale, maxitr, center, exponent, width, height))
+        .flatten()
+        .collect();
+    buf
+}
+fn findcolour(iterations: i32, maxitr: f64, r: f64) -> [u8; 4] {
+    if iterations >= maxitr as i32 {
+        return [0, 0, 0, 255];
+    }
+    let iterations: f64 = iterations as f64 + 1.0 - (((r).ln() / 2.0).ln()) / 2.0_f64.ln();
+    let mut hsl = Hsl::default();
+    hsl.set_hue((360.0 * (iterations / maxitr)) % 360.0);
+    hsl.set_saturation(100.);
+    hsl.set_lightness(50.0);
+    let rgb_arr: [u8; 3] = Rgb::from(&hsl).into();
+    let mut rgba: [u8; 4] = [0; 4];
+    rgba[0] = rgb_arr[0];
+    rgba[1] = rgb_arr[1];
+    rgba[2] = rgb_arr[2];
+    rgba[3] = 255;
+    rgba
+}
+fn px(x: f64, scale: f64, oX: f64, width: i32) -> f64 {
+    return (oX) + ((2.0 * ((x - 1.0) / (width as f64 - 1.0)) - 1.0) * 1.235 * scale);
+}
+fn py(y: f64, scale: f64, oY: f64, height: i32) -> f64 {
+    return (oY + (2.0 * ((y - 1.0) / (height as f64 - 1.0)) - 1.0) * 1.12 * scale);
+}
+fn mandel2(mut x0: f64, mut y0: f64, maxitr: f64) -> (i32, f64) {
+    let mut iterations: i32 = 0;
+    let mut x2: f64 = 0.0;
+    let mut y2: f64 = 0.0;
+    let mut x: f64 = 0.0;
+    let mut y: f64 = 0.0;
+    while x2 + y2 < 4.0 && iterations < maxitr as i32 {
+        y = 2.0 * x * y + y0;
+        x = x2 - y2 + x0;
+        x2 = x * x;
+        y2 = y * y;
+        iterations = iterations + 1;
+    }
+    return (iterations, x2 + y2);
+}
+fn renderline(
+    linenumber: u32,
+    scale: f64,
+    maxitr: f64,
+    center: coord,
+    exponent: i32,
+    width: i32,
+    height: i32,
+) -> (Vec<u8>) {
+    let mut x0: f64;
+    let mut line: Vec<u8> = Vec::new();
+    let y0 = py((linenumber as f64), scale, center.y, height);
+    for x in 0..width {
+        x0 = px(x as f64, scale, center.x, width);
+        let (iterations, r) = mandelcomp(x0, y0, maxitr, exponent);
+        line.extend_from_slice(&findcolour(iterations, maxitr, r));
+    }
+    return line;
+}
+fn mandelcomp(mut x0: f64, mut y0: f64, maxitr: f64, n: i32) -> (i32, f64) {
+    if n == 2 {
+        return mandel2(x0, y0, maxitr);
+    }
+    let mut iterations: i32 = 0;
+    let c = complex { re: x0, im: y0 };
+    let mut z: complex = complex { re: 0., im: 0. };
+    while z.re * z.re + z.im * z.im < 4. && iterations < maxitr as i32 {
+        z = cadd(cpow(z, n), c);
+        iterations += 1;
+    }
+    return (iterations, z.re * z.re + z.im * z.im);
+}
